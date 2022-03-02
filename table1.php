3d8a2a2b56891d9a6f07ea0377d73dcd664be3e6
FightPJ <97584439+FightPJ@users.noreply.github.com>
2022-03-02 13:37:57 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -8,14 +8,38 @@
         <th>ลำดับ</th>
         <th>ชื่อ</th>
         <th>รูปภาพ</th>
-        <th>Twitter/Fb</th>
+        <th>Twitter/FB</th>
       </tr>
       <tr>  
         <td>1</td>
-        <td>หัฏฐกร ประจง</td>
-        <td><img src="https://scontent.fphs4-1.fna.fbcdn.net/v/t39.30808-6/274624682_1176668316474775_3249687038144962466_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFZ3CBC8ofmOAJEu6fpm0N0qBCqkx2Z5YCoEKqTHZnlgIrJX3WwssC0H2ccFv7pdaZUXFvvxO60z-FCoTwYxAF2&_nc_ohc=G7gofIHb_LEAX_xZYaD&_nc_ht=scontent.fphs4-1.fna&oh=00_AT9vrU8i8i7saw_KgSXgHmiJJ0CGKaKMagSXrjI_5d3dug&oe=6223A996" width="45px"></td>
-        <td><a href="https://www.facebook.com/fight.prajong.3">Facebook</a></td>
-        <tr>
+        <td>..</td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Facebook</a></td>
+      </tr>
+      <tr>  
+        <td>2</td>
+        <td>..</td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Facebook</a></td>
+      </tr>
+      <tr>  
+        <td>3</td>
+        <td>..</td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Facebook</a></td>
+      </tr>
+      <tr>  
+        <td>4</td>
+        <td>..</td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Facebook</a></td>
+      </tr>
+      <tr>  
+        <td>5</td>
+        <td>..</td>
+        <td><img src="" width="45px"></td>
+        <td><a href="">Facebook</a></td>
+      </tr>
     </table>
   </body>
 </html>
